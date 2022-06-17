51a923bc8dae01c7f6cdbbfea522a48b731f771e
satyapatel293 <satyapatel293@gmail.com>
2022-06-17 14:44:50 -0700

    Create function that plays 1 round

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ function computerPlay () {
   (x === 2) ? "paper" : "scissors" 
 }
 
+function playRound (playerSelector, computerSelector) {
+  if (playerSelector === "paper" &&  computerSelector === "rock") {
+  return "You Win! Paper Beats Rock"
+  }
+  else if (playerSelector == "paper" &&  computerSelector == "scissors") {
+    return "You Lose! Scissors Beats Paper"
+  }
+  else if (playerSelector == "paper" &&  computerSelector == "paper") {
+    return "Draw!"
+  }
+  else if (playerSelector == "rock" &&  computerSelector == "scissors") {
+    return "You Win! Rock Beats Scissors"
+  }
+  else if (playerSelector == "rock" &&  computerSelector == "paper") {
+    return "You Lose! Paper Beats Rock"
+  }
+  else if (playerSelector == "rock" &&  computerSelector == "rock") {
+    return "Draw!"
+  }
+  else if (playerSelector == "scissors" &&  computerSelector == "paper") {
+    return "You Win! Scissors Beats Paper"
+    
+  }
+  else if (playerSelector == "scissors" &&  computerSelector == "rock") {
+    return "You Lose! Rock Beats Scissors"
+  }
+  else if (playerSelector == "scissors" &&  computerSelector == "scissors") {
+    return "Draw!"
+  }
+}
